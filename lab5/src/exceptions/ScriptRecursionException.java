33cc971f1064f5b101528e9c0b596d1163f4df2b
aoswhiskey <ilyapashv@gmail.com>
2024-09-14 02:36:02 +0300

    Delete lab5 directory

--- lab5/src/exceptions/ScriptRecursionException.java
+++
@@ -1,5 +0,0 @@
-package exceptions;
-/**
- * Выбрасывается, если происходит рекурсия вызывания скрипта
- */
-public class ScriptRecursionException extends Exception {}
--- lab5/src/models/ask/AskBreak.java
+++
@@ -1,6 +0,0 @@
-package models.ask;
-/**
- * Выбрасывается, если прописывается команда 'exit' в момент ввода данных.
- */
-public class AskBreak extends Exception {
-}
